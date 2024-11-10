e09e83620ee8a215a5140fc3eefcbb14e09d1348
Rayara Sousa Carvalho <rayara21.sousa@hotmail.com>
2024-11-10 20:50:41 -0300

    Correções gerais

--- paginas/divisao/divisao.js
+++ paginas/divisao/divisao.js
@@ -1,14 +1,14 @@
 const operacoes = [
     { num1: 72, num2: 6, resultado: 12, simbolo: "/", url: "./../../assets/vídeos/(12)doze.mp4" },
     { num1: 180, num2: 9, resultado: 20, simbolo: "/", url: "./../../assets/vídeos/(20)vinte.mp4" },
-    { num1: 154, num2: 7, resultado: 22, simbolo: "/", url: "./../../assets/vídeos/(25)vinte-e-cinco.mp4" },
+    { num1: 154, num2: 7, resultado: 22, simbolo: "/", url: "./../../assets/vídeos/(22)vinte-e-dois.mp4" },
     { num1: 144, num2: 6, resultado: 24, simbolo: "/", url: "./../../assets/vídeos/(24)vinte-e-quatro.mp4" },
     { num1: 100, num2: 4, resultado: 25, simbolo: "/", url: "./../../assets/vídeos/(25)vinte-e-cinco.mp4" },
     { num1: 90, num2: 3, resultado: 30, simbolo: "/", url: "./../../assets/vídeos/(30)trinta.mp4" },
     { num1: 128, num2: 4, resultado: 32, simbolo: "/", url: "./../../assets/vídeos/(32)trinta-e-dois.mp4" },
     { num1: 200, num2: 5, resultado: 40, simbolo: "/", url: "./../../assets/vídeos/(40)quarenta.mp4" },
-    { num1: 300, num2: 5, resultado: 60, simbolo: "/", url: "./../../assets/vídeos/(30)trinta.mp4" },
-    { num1: 376, num2: 4, resultado: 94, simbolo: "/", url: "./../../assets/vídeos/(30)trinta.mp4" },
+    { num1: 300, num2: 5, resultado: 60, simbolo: "/", url: "./../../assets/vídeos/(60)sessenta.mp4" },
+    { num1: 376, num2: 4, resultado: 94, simbolo: "/", url: "./../../assets/vídeos/(94)noventa-e-quatro.mp4" },
   ];
   
   iniciarOperacoes(operacoes)
